187c9dac02f49f8c173eea03c80710de12a956fa
Timur <timur199382@gmail.com>
2019-04-12 00:11:01 +0500

    Сделано 4-5

---
+++ Lab_5_Frontend/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Барбершоп NORECK77</title>
+	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<a href="client.php" class="subjects">
+			<div class="sub_sub">
+				<i class="fas fa-users"></i>
+				<p>Клиенты</p>
+			</div>
+		</a>
+		<a href="services.php" class="subjects">
+			<div class="sub_sub">
+				<i class="fas fa-cut"></i>
+				<p>Услуги</p>
+			</div>
+		</a>
+		<a href="requests.php" class="subjects">
+			<div class="sub_sub">
+				<i class="fas fa-paper-plane"></i>
+				<p>Заявки</p>
+			</div>
+		</a>
+		<a href="request_contain.php" class="subjects">
+			<div class="sub_sub">
+				<i class="fas fa-list"></i>
+				<p>Содержание заявки</p>
+			</div>
+		</a>
+	</div>
+
+	
+	<a href="REST_API_FROM4.php" style="position: absolute; background-color: #dff441; text-decoration: none; padding: 15px; font-size: 3.2vh; border-radius: 5px; color: #000; text-align: center; top: 70%; left: 43vw;">
+			<div class="sub_sub">
+				<i class="fas fa-list"></i>
+				<p>Вызов REST API</p>
+			</div>
+	</a>
+
+</body>
+</html>
